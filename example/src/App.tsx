d425ef4740f928a51ee1dd990960f03d5ef2c60a
Neelav Rana <neelav@duettoresearch.com>
2020-08-01 16:19:04 -0700

    scaffolding of app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,79 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Panel } from "primereact/panel";
+import "./App.css";
+import "primereact/resources/primereact.css";
+import "primereact/resources/themes/nova-light/theme.css";
+import "primeflex/primeflex.css";
+import {
+  RequirementsConverter,
+  DefaultEntityRegistry,
+} from "stripe-onboarding-schema";
+import Stripe from "stripe";
 
-function App() {
-  return (
-    <div className="App">
+const entityRegistry = DefaultEntityRegistry.make();
+const requirementsConverter = new RequirementsConverter(entityRegistry);
 
-    </div>
-  );
+type State = {
+  requirements?: string | undefined;
+  requirementsType: "past_due" | "currently_due" | "eventually_due";
+};
+
+class App extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      requirementsType: "past_due",
+    };
+  }
+
+  render() {
+    let uiSchemaJson;
+    if (this.state.requirements) {
+      try {
+        const parsedRequirements = JSON.parse(
+          this.state.requirements
+        ) as Stripe.Account.Requirements;
+
+        const schema = requirementsConverter.convertRequirements(
+          parsedRequirements,
+          this.state.requirementsType
+        );
+        console.log(schema);
+        uiSchemaJson = JSON.stringify(schema, null, 4);
+      } catch (e) {
+        uiSchemaJson = "Invalid requirements json";
+      }
+    }
+
+    return (
+      <div className="App">
+        <div className="p-grid p-align-stretch vertical-container">
+          <div className="p-col">
+            <Panel header="Requirements">
+              <textarea
+                name="requirements"
+                style={{ width: "100%", height: "100%" }}
+                placeholder="Enter an account api requirements hash..."
+                value={this.state.requirements}
+                onChange={(event) =>
+                  this.setState({
+                    ...this.state,
+                    requirements: event.target.value,
+                  })
+                }
+              ></textarea>
+            </Panel>
+          </div>
+          <div className="p-col">
+            <Panel header="UI Schema">{uiSchemaJson}</Panel>
+          </div>
+          <div className="p-col">
+            <Panel header="UI Form"></Panel>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
